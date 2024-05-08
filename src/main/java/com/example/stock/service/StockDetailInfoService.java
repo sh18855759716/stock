364b46573bcbfa59a2d4e5ee233026187106753b
yt-confirm <68738517+yt-confirm@users.noreply.github.com>
2024-05-08 13:44:35 +0800

    Update StockDetailInfoService.java

--- src/main/java/com/example/stock/service/StockDetailInfoService.java
+++ src/main/java/com/example/stock/service/StockDetailInfoService.java
@@ -1,6 +1,13 @@
 package com.example.stock.service;
 
 
+import com.baomidou.mybatisplus.service.IService;
+import com.example.stock.entity.OrderProductInfoEntity;
+import com.example.stock.entity.StockDetailInfoEntity;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
 /**
  * 库存明细表
  *
@@ -9,7 +16,17 @@ package com.example.stock.service;
  * @date 2024-04-30 13:57:20
  * @version v1.1
  */
-public interface StockDetailInfoService {
+public interface StockDetailInfoService extends IService<StockDetailInfoEntity> {
+
+    /**
+     * 查询订单出入库数量
+     * @param type
+     * @param documentNo
+     * @return
+     */
+    Map<Long, BigDecimal> queryProductQuantity(Integer type, String documentNo);
+
+
 
 
 }
