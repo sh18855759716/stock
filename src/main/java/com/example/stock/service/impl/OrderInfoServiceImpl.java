08f62cdb77ae4b03a61bc9f6816b5a3e07783c99
yt-confirm <68738517+yt-confirm@users.noreply.github.com>
2024-05-08 13:43:12 +0800

    Update OrderInfoServiceImpl.java

--- src/main/java/com/example/stock/service/impl/OrderInfoServiceImpl.java
+++ src/main/java/com/example/stock/service/impl/OrderInfoServiceImpl.java
@@ -4,6 +4,7 @@ package com.example.stock.service.impl;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.NumberUtil;
 import com.alibaba.excel.util.StringUtils;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.pagination.PageHelper;
@@ -11,26 +12,37 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.example.stock.dao.OrderInfoDao;
 import com.example.stock.dto.QueryOrderInfoPageDto;
 import com.example.stock.dto.SaveOrderInfoDto;
+import com.example.stock.dto.SaveStockDetailDto;
+import com.example.stock.dto.SaveStockDto;
 import com.example.stock.entity.OrderInfoEntity;
 import com.example.stock.entity.OrderProductInfoEntity;
 import com.example.stock.entity.ProductManagementInfoEntity;
+import com.example.stock.entity.StockDetailInfoEntity;
+import com.example.stock.entity.StockInfoEntity;
 import com.example.stock.service.OrderInfoService;
 import com.example.stock.service.OrderProductInfoService;
 import com.example.stock.service.ProductManagementInfoService;
+import com.example.stock.service.StockDetailInfoService;
+import com.example.stock.service.StockInfoService;
+import com.example.stock.util.IdNumberUtil;
 import com.example.stock.vo.ApiUtil;
 import com.example.stock.vo.BaseApi;
 import com.example.stock.vo.OrderInfoVo;
 import com.example.stock.vo.PageVo;
 import com.github.pagehelper.PageInfo;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
@@ -50,6 +62,15 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoDao, OrderInfoEnt
 
     @Resource
     ProductManagementInfoService productManagementInfoService;
+
+    @Resource
+    StockDetailInfoService stockDetailInfoService;
+
+    @Resource
+    StockInfoService stockInfoService;
+
+    @Autowired
+    private IdNumberUtil idNumberUtil;
 
     @Override
     public BaseApi<PageVo<OrderInfoEntity>> queryOrderInfoPage(Integer orderType, QueryOrderInfoPageDto dto) {
@@ -82,6 +103,15 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoDao, OrderInfoEnt
         Long id = dto.getId();
         Date now = new Date();
         String resStr = dto.getOrderNum();
+
+        // 订单明细
+        List<OrderProductInfoEntity> orderProductInfoEntitieList = dto.getOrderProductInfoEntitieList();
+        List<Long> collect = orderProductInfoEntitieList.stream().map(OrderProductInfoEntity::getProductInfoId).distinct().collect(Collectors.toList());
+        if (collect.size() != orderProductInfoEntitieList.size()) {
+            res.paramError("订单的产品不允许重复", null);
+            return res;
+        }
+
 
         OrderInfoEntity entity = new OrderInfoEntity();
         // 新增
@@ -116,8 +146,7 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoDao, OrderInfoEnt
 
         //orderProductInfoService.delete(new EntityWrapper<OrderProductInfoEntity>()
         //		.eq("order_info_id", id));
-        // 订单明细
-        List<OrderProductInfoEntity> orderProductInfoEntitieList = dto.getOrderProductInfoEntitieList();
+
         BigDecimal productNumber = orderProductInfoEntitieList.stream().map(OrderProductInfoEntity::getProductNumber).reduce(BigDecimal.ZERO, BigDecimal::add);
         BigDecimal money = orderProductInfoEntitieList.stream().map(OrderProductInfoEntity::getProductMoney).reduce(BigDecimal.ZERO, BigDecimal::add);
 
@@ -185,10 +214,106 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoDao, OrderInfoEnt
     }
 
     @Override
-    public BaseApi<?> saveInOrOutbound(Long id) {
+    @Transactional
+    public BaseApi<?> saveInOrOutbound(SaveStockDto dto) {
+        BaseApi<?> res = new BaseApi<>();
+        OrderInfoEntity entity = selectById(dto.getId());
+        String message = entity.getOrderType() == 1 ? "入库" : "出库";
+        List<SaveStockDetailDto> detailDtoList = dto.getDetailDtoList();
+        List<OrderProductInfoEntity> orderProductInfoEntities = orderProductInfoService.selectList(new EntityWrapper<OrderProductInfoEntity>()
+                .eq("order_info_id", dto.getId()));
+
+        Map<Long, BigDecimal> orderProductMap = orderProductInfoEntities.stream().collect(Collectors.toMap(OrderProductInfoEntity::getProductInfoId, OrderProductInfoEntity::getProductNumber));
+
+
+        Map<Long, BigDecimal> productQuantityMap = stockDetailInfoService.queryProductQuantity(entity.getOrderType(), entity.getOrderNum());
+        for (SaveStockDetailDto saveStockDetailDto : detailDtoList) {
+            Long productId = saveStockDetailDto.getProductId();
+            BigDecimal quantity = saveStockDetailDto.getQuantity();
+            BigDecimal oldQuantity = productQuantityMap.get(productId);
+            BigDecimal total = orderProductMap.get(productId);
+            total = Optional.ofNullable(total).orElse(BigDecimal.ZERO);
+            if (total.compareTo(NumberUtil.add(quantity, oldQuantity)) < 0) {
+                res.paramError(message + "数量超过订单总量", null);
+                return res;
+            }
+        }
+
+        for (SaveStockDetailDto saveStockDetailDto : detailDtoList) {
+            StockDetailInfoEntity stockDetailInfo = new StockDetailInfoEntity();
+            stockDetailInfo.setProductInfoId(saveStockDetailDto.getProductId());
+            stockDetailInfo.setSaleOrderNum(entity.getOrderType() == 1 ? null : entity.getOrderNum());
+            stockDetailInfo.setPurchaseOrderNum(entity.getOrderType() == 1 ? entity.getOrderNum() : null);
+            stockDetailInfo.setOperateType(entity.getOrderType());
+            stockDetailInfo.setStockNum(dto.getStockNum());
+            stockDetailInfo.setOperateNumber(saveStockDetailDto.getQuantity());
+            stockDetailInfo.setCreateTime(new Date());
+            stockDetailInfo.setUpdateTime(new Date());
+            stockDetailInfoService.insert(stockDetailInfo);
+
+            BigDecimal direction = entity.getOrderType() == 1 ? new BigDecimal("1") : new BigDecimal("-1");
+            BigDecimal quantity = NumberUtil.mul(saveStockDetailDto.getQuantity(), direction);
+            StockInfoEntity stockInfoEntity = stockInfoService.selectOne(new EntityWrapper<StockInfoEntity>()
+                    .eq("product_info_id", saveStockDetailDto.getProductId())
+                    .eq("is_del", 0));
+            if (stockInfoEntity == null) {
+                stockInfoEntity = new StockInfoEntity();
+                stockInfoEntity.setProductInfoId(saveStockDetailDto.getProductId());
+                stockInfoEntity.setCreateTime(new Date());
+            }
+            stockInfoEntity.setStockNumber(NumberUtil.add(quantity, stockInfoEntity.getStockNumber()));
+            stockInfoEntity.setUpdateTime(new Date());
+            stockInfoService.insertOrUpdate(stockInfoEntity);
+        }
+        res.success(null);
+        return res;
+    }
+
+    @Override
+    public BaseApi<OrderInfoVo> queryOrderSerialInfo(Long id) {
+        BaseApi<OrderInfoVo> res = new BaseApi<>();
         OrderInfoEntity entity = selectById(id);
-        // TODO 操作出库入库动作,等需求回复
-        return null;
+        Integer orderType = entity.getOrderType();
+        List<OrderProductInfoEntity> orderProductInfoEntities = orderProductInfoService.selectList(new EntityWrapper<OrderProductInfoEntity>()
+                .eq("order_info_id", id));
+
+        OrderInfoVo vo = new OrderInfoVo();
+        BeanUtil.copyProperties(entity, vo, CopyOptions.create().ignoreError());
+        vo.setStockNum(idNumberUtil.getStockNumber(orderType));
+        Map<Long, BigDecimal> longBigDecimalMap = stockDetailInfoService.queryProductQuantity(orderType, entity.getOrderNum());
+        // 获取产品明细
+        List<Long> productIdList = orderProductInfoEntities.stream().map(OrderProductInfoEntity::getProductInfoId).collect(Collectors.toList());
+        Map<Long, ProductManagementInfoEntity> productManagementInfoEntityMap = productManagementInfoService.queryProductMap(productIdList);
+        for (OrderProductInfoEntity orderProductInfoEntity : orderProductInfoEntities) {
+            Long productInfoId = orderProductInfoEntity.getProductInfoId();
+            if(productManagementInfoEntityMap.containsKey(productInfoId)){
+                ProductManagementInfoEntity productManagementInfoEntity = productManagementInfoEntityMap.get(productInfoId);
+                orderProductInfoEntity.setProductNum(productManagementInfoEntity.getProductNum());
+                orderProductInfoEntity.setProductName(productManagementInfoEntity.getProductName());
+                orderProductInfoEntity.setSpecification(productManagementInfoEntity.getSpecification());
+            }
+            if (longBigDecimalMap.containsKey(productInfoId)) {
+                BigDecimal bigDecimal = longBigDecimalMap.get(productInfoId);
+                orderProductInfoEntity.setQuantity(bigDecimal);
+            }
+        }
+
+        vo.setOrderProductInfoEntitieList(orderProductInfoEntities);
+
+        res.success(vo);
+        return res;
+    }
+
+    @Override
+    public List<ProductManagementInfoEntity> fuzzyQueryOrderProductList(Long id, String productName) {
+
+        List<OrderProductInfoEntity> orderProductInfoEntities = orderProductInfoService.selectList(new EntityWrapper<OrderProductInfoEntity>()
+                .eq("order_info_id", id));
+        List<Long> collect = orderProductInfoEntities.stream().map(OrderProductInfoEntity::getProductInfoId).collect(Collectors.toList());
+        if (CollectionUtils.isEmpty(collect)) {
+            return new ArrayList<>();
+        }
+        return productManagementInfoService.fuzzyQueryProductList(collect, productName);
     }
 
     /**
