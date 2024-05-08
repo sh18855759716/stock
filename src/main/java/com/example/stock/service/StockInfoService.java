26e2c5fcf660bb8e70bbb3f582559fa3949a35ea
yt-confirm <68738517+yt-confirm@users.noreply.github.com>
2024-05-08 13:44:49 +0800

    Update StockInfoService.java

--- src/main/java/com/example/stock/service/StockInfoService.java
+++ src/main/java/com/example/stock/service/StockInfoService.java
@@ -1,7 +1,10 @@
 package com.example.stock.service;
 
 
+import com.baomidou.mybatisplus.service.IService;
 import com.example.stock.dto.StockInfoDTO;
+import com.example.stock.entity.StockDetailInfoEntity;
+import com.example.stock.entity.StockInfoEntity;
 import com.example.stock.vo.PageBean;
 
 /**
@@ -12,7 +15,7 @@ import com.example.stock.vo.PageBean;
  * @date 2024-04-30 13:57:20
  * @version v1.1
  */
-public interface StockInfoService {
+public interface StockInfoService extends IService<StockInfoEntity> {
 
 
     PageBean queryStockInfoList(StockInfoDTO stockInfoDTO);
